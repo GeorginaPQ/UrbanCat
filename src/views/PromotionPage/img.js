5c24a0429331bdcd8571572200bada75c17ec59b
GeorginaPQ <giorginapqe@gmail.com>
2020-06-12 02:44:24 -0500

    :bento: Add array promotion

---
+++ src/views/PromotionPage/img.js
@@ -0,0 +1,17 @@
+import Promotion1 from '../../assets/Images/promotion1.png';
+import Promotion2 from '../../assets/Images/promotion2.png';
+import Promotion3 from '../../assets/Images/promotion3.png';
+import Promotion4 from '../../assets/Images/promotion4.png';
+import Promotion5 from '../../assets/Images/promotion5.png';
+import Promotion6 from '../../assets/Images/promotion6.png';
+
+
+
+export const ImagePromotion = [
+  Promotion1,
+  Promotion2,
+  Promotion3,
+  Promotion4,
+  Promotion5,
+  Promotion6  
+]
